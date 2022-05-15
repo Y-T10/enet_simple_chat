db84a7428042edfbe680f50e7465771151a23dc6
Y-T10 <tayuji111@gmail.com>
2022-05-15 12:45:40 +0900

    パケット一斉送信関数を追加

--- enet_send.cpp
+++ enet_send.cpp
@@ -1,5 +1,7 @@
 #include"enet_send.hpp"
 
+#include<ranges>
+
 const bool Send_ENet_Packet(ENetPeer* dst, const size_t channel, const void* data, const size_t data_size, const bool is_reliable){
     const enet_uint32 flag = is_reliable ? ENET_PACKET_FLAG_RELIABLE : ENET_PACKET_FLAG_UNRELIABLE_FRAGMENT;
     ENetPacket *packet = enet_packet_create(data, data_size, flag);
@@ -10,5 +12,19 @@ const bool Send_ENet_Packet(ENetPeer* dst, const size_t channel, const void* dat
         enet_packet_destroy(packet);
         return false;
     }
+    return true;
+}
+
+const bool Broadcast_ENet_Packet(ENetHost* server, const size_t channel, const void* data, const size_t data_size, const bool is_reliable){
+    if(server == NULL){
+        return false;
+    }
+
+    const enet_uint32 flag = is_reliable ? ENET_PACKET_FLAG_RELIABLE : ENET_PACKET_FLAG_UNRELIABLE_FRAGMENT;
+    ENetPacket *packet = enet_packet_create(data, data_size, flag);
+    if(packet == NULL){
+        return false;
+    }
+    enet_host_broadcast(server, channel, packet);
     return true;
 }
