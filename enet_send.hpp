a8ad70f4613ab03f433f4aa97dc8842b8be99c59
Y-T10 <tayuji111@gmail.com>
2022-05-21 13:29:05 +0900

    vector用の送信関数を追加

--- enet_send.hpp
+++ enet_send.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include<enet/enet.h>
+#include<cstddef>
+#include<vector>
 
 /**
  * パケット送信関数
@@ -12,6 +14,7 @@
  * @return 送信の成否
  */
 const bool Send_ENet_Packet(ENetPeer* dst, const size_t channel, const void* data, const size_t data_size, const bool is_reliable);
+const bool Send_ENet_Packet(ENetPeer* dst, const size_t channel, const std::vector<std::byte>& data, const bool is_reliable);
 
 /**
  * パケット一斉送信関数
