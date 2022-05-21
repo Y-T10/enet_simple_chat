c43597d7afd57ba92c62c78906b6a40f805b66d9
Y-T10 <tayuji111@gmail.com>
2022-05-21 14:19:53 +0900

    コンフリクトを修正し、main関数内をchat_systemで再実装した

--- client.cpp
+++ client.cpp
@@ -206,7 +206,8 @@ class chat_system : public Meditator , private boost::noncopyable {
     chat_system() noexcept
     :m_io(nullptr)
     ,m_communicate(nullptr)
-    ,m_quit_flag(false){
+    ,m_quit_flag(false)
+    ,m_username("default_name"){
         create_colleagues();
     }
 
@@ -222,7 +223,8 @@ class chat_system : public Meditator , private boost::noncopyable {
         m_communicate->set_meditator(this);
     };
 
-    const bool initilize(){
+    const bool initilize(const std::string& usename){
+        m_username = usename;
         return m_communicate->try_connection(HOST, PORT);
     }
 
@@ -241,8 +243,7 @@ class chat_system : public Meditator , private boost::noncopyable {
             const auto event = m_communicate->lastEvent();
             if(event == ENET_EVENT_TYPE_CONNECT){
                 assert(m_communicate->isConnected());
-                const auto username = std::string("default_name");
-                const auto msg_vector = std::vector<std::byte>(username.begin(), username.end());
+                const auto msg_vector = std::vector<std::byte>(m_username.begin(), m_username.end());
                 m_communicate->add_sent_data(msg_vector, 0);
                 m_communicate->flush();;
                 return;
@@ -275,10 +276,11 @@ class chat_system : public Meditator , private boost::noncopyable {
     std::unique_ptr<chat_io> m_io;
     std::unique_ptr<chat_communication> m_communicate;
     bool m_quit_flag;
+    ///本当はColleagueに封じ込めるべきだが面倒なのでこのようにした。
+    std::string m_username;
 };
 
 int  main(int argc, char ** argv) {
-    int connected=0;
     if (argv[1] == NULL) {
         printf("Usage: client username\n");
         exit(1);
@@ -290,63 +292,20 @@ int  main(int argc, char ** argv) {
         return 0;
     }
 
-    ENetHost *client = enet_host_create(NULL, 1, 2, 5760/8, 1440/8);
-    if (client == NULL) {
-        printf("Could not create client.\n");
-        return 0;
+    //チャットシステムを作成する
+    auto chat_sys = std::make_unique<chat_system>();
+    if(!chat_sys->initilize(argv[1])){
+        chat_sys = nullptr;
+        enet_deinitialize();
+        return 1;
     }
 
-    ENetAddress address;
-    enet_address_set_host(&address, HOST);
-    address.port = PORT;
-
-    ENetPeer *peer = enet_host_connect(client, &address, 2, 0);
-    if (peer == NULL) {
-        printf("Could not connect to server\n");
-        return 0;
+    while(!chat_sys->is_quit()){
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        chat_sys->update();
     }
 
-    ENetEvent event;
-    if (enet_host_service(client, &event, 1000) > 0 && event.type == ENET_EVENT_TYPE_CONNECT) {
-        printf("Connection to %s succeeded.\n", HOST);
-        connected = 1;
-        Send_ENet_Packet(peer, 0, argv[1], strlen(argv[1])+1, true);
-        enet_host_flush(client);
-    } else {
-        enet_peer_reset(peer);
-        printf("Could not connect to %s.\n", HOST);
-        return 0;
-    }
-
-    while (enet_host_service(client, &event, 1000) >= 0) {
-        if(event.type == ENET_EVENT_TYPE_RECEIVE){
-            printf("%s\n", (char*) event.packet->data);
-            enet_packet_destroy(event.packet);
-            continue;
-        }
-        if(event.type == ENET_EVENT_TYPE_DISCONNECT){
-            connected = 0;
-            printf("You have been disconnected.\n");
-            break;
-        }
-        if(event.type == ENET_EVENT_TYPE_NONE){
-            if (connected) {
-                //printf("Input: ");
-                const char *buffer = "hi all";
-                //char  buffer[BUFFERSIZE] = { 0 };
-                //scanf("%[^\n]%*c", buffer);
-                if (strlen(buffer) == 0) { continue; }
-                if (strlen(buffer) == 1 && buffer[0] == 'q') {
-                    connected = 0;
-                    enet_peer_disconnect(peer, 0);
-                    continue;
-                }
-                Send_ENet_Packet(peer, 0, buffer, strlen(buffer)+1, false);
-            }
-        }
-    }
-
-    enet_peer_reset(peer);
-    enet_host_destroy(client);
+    chat_sys = nullptr;   
     enet_deinitialize();
+    return 0;
 }
