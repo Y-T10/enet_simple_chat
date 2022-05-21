44e2d46b9141b9e389f80cfc0e4892b7a3e5a00b
Y-T10 <tayuji111@gmail.com>
2022-05-21 14:36:56 +0900

    string送信関数を追加

--- enet_send.cpp
+++ enet_send.cpp
@@ -19,6 +19,10 @@ const bool Send_ENet_Packet(ENetPeer* dst, const size_t channel, const std::vect
     return Send_ENet_Packet(dst, channel, data.data(), data.size(), is_reliable);
 }
 
+const bool Send_ENet_Packet(ENetPeer* dst, const size_t channel, const std::string& data, const bool is_reliable){
+    return Send_ENet_Packet(dst, channel, data.data(), data.size(), is_reliable);
+}
+
 const bool Broadcast_ENet_Packet(ENetHost* server, const size_t channel, const void* data, const size_t data_size, const bool is_reliable){
     if(server == NULL){
         return false;
