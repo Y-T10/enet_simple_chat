4c6632ac0fdaa28948e75fe984b266673a59658d
Y-T10 <tayuji111@gmail.com>
2022-05-27 11:58:58 +0900

    メインループをクラスで置き換えた

--- server.cpp
+++ server.cpp
@@ -9,6 +9,7 @@
 #include<memory>
 #include<optional>
 #include<list>
+#include<thread>
 
 using namespace std;
 
@@ -272,52 +273,12 @@ int  main(int argc, char ** argv) {
       printf("Could not initialize enet.");
       return 0;
     }
-    const ENetAddress address = {.host = ENET_HOST_ANY, .port = PORT};
-    ENetHost *server = enet_host_create(&address, 100, 2, 0, 0);
-    if(server == NULL) {
-      printf("Could not start server.\n");
-      return 0;
+
+    auto m_server_sys = std::make_unique<server_system>();
+    while(true){
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        m_server_sys->update();
     }
 
-    ENetEvent event;
-    while (enet_host_service(server, &event, 1000) >= 0) {
-        if(event.type == ENET_EVENT_TYPE_CONNECT){
-            const enet_uint32 ip = event.peer->address.host;
-            const enet_uint16 port = event.peer->address.port;
-            fprintf(stderr, "[connection] ip: %x, port: %u\n", ip, port);
-            continue;
-        }
-        if(event.type == ENET_EVENT_TYPE_DISCONNECT){
-            fprintf(stderr, "[disconnect] \"%s\" disconnect.\n", (char*)event.peer->data);
-            char buffer[BUFFERSIZE] = { 0 };
-            sprintf(buffer, "%s has disconnected.", (char*)event.peer->data);
-            Broadcast_ENet_Packet(server, 0, buffer, strlen(buffer)+1, false);
-            enet_host_flush(server);
-            free(event.peer->data);
-            event.peer->data = NULL;
-            continue;
-        }
-        if(event.type == ENET_EVENT_TYPE_RECEIVE){
-            PrintPacket(event.packet);
-            if (event.peer->data == NULL) {
-                fprintf(stderr, "[join] \"%s\" joined.\n", event.packet->data);
-                char buffer[BUFFERSIZE] = { 0 };
-                event.peer->data = malloc(strlen((char*) event.packet->data)+1);
-                strcpy((char*) event.peer->data, (char*) event.packet->data);
-                sprintf(buffer, "%s has connected\n", (char*) event.packet->data);
-                Broadcast_ENet_Packet(server, 0, buffer, strlen(buffer)+1, false);
-                enet_host_flush(server);
-            } else {
-                char buffer[BUFFERSIZE] = { 0 };
-                sprintf(buffer, "%s: %s", (char*) event.peer->data, (char*)event.packet->data);
-                Broadcast_ENet_Packet(server, 0, buffer, strlen(buffer)+1, false);
-                enet_host_flush(server);
-                fprintf(stderr, "[message] %s\n", buffer);
-            }
-            enet_packet_destroy(event.packet);
-            continue;
-        }
-    }
-    enet_host_destroy(server);
     enet_deinitialize();
 }
