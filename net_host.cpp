48c9d78456aade557b1cf03e2fa126f7d4e2a7ec
Y-T10 <tayuji111@gmail.com>
2022-09-07 19:24:30 +0900

    ENetのホストを扱うクラスを追加

---
+++ net_host.cpp
@@ -0,0 +1,83 @@
+#pragma once
+
+#include"net_host.hpp"
+#include<cassert>
+
+NetHost::NetHost():
+m_host(nullptr){};
+
+NetHost::~NetHost(){
+    if(m_host != NULL){
+        enet_host_destroy(m_host);
+    }
+}
+
+const bool NetHost::set_host(const HostSetting& s){
+    clear_host();
+    m_host = enet_host_create(&(s.address), s.peer_max, s.packet_ch_max, s.down_size, s.up_size);
+    return m_host != NULL;
+}
+
+void NetHost::clear_host(){
+    if(m_host == NULL){
+        return;
+    }
+    enet_host_destroy(m_host);
+    m_host = NULL;
+}
+
+NetHost::operator bool(){
+    return m_host != nullptr;
+};
+
+const bool NetHost::request_connection(const ENetAddress* dst, const size_t max_ch, const enet_uint32 data){
+    assert(*this);
+    assert(dst);
+    return enet_host_connect(m_host, dst, max_ch, data) != NULL;
+}
+
+const std::optional<ENetPeer*> find_peer(
+const ENetHost *host,
+const std::function<const bool(const ENetPeer*)>& finder){
+    assert(host);
+    const auto begin = host->peers;
+    const auto end   = host->peers + host->peerCount;
+    const auto target = std::find_if(begin, end, finder);
+    if(target == end){
+        return std::nullopt;
+    }
+    return target;
+}
+
+const bool NetHost::is_there_peer(const std::function<const bool(const ENetPeer*)>& finder){
+    return find_peer(m_host, finder).has_value();
+}
+
+void NetHost::handle_peer(
+const std::function<const bool(const ENetPeer*)>& finder,
+const std::function<void(ENetPeer*)>& peer_handler){
+    assert(*this);
+    auto target = find_peer(m_host, finder);
+    if(target){
+        peer_handler(*target);
+    }
+}
+
+const bool NetHost::handle_host_event(const std::function<const bool(const ENetEvent*)>& event_handler){
+    assert(*this);
+    //エラーが生じるまでイベントを処理する
+    ENetEvent e;
+    if(enet_host_check_events(m_host, &e) < 0){
+        return false;
+    }
+    return event_handler(&e);
+}
+
+void NetHost::broadcast(const size_t ch, ENetPacket* packet){
+    assert(*this);
+    enet_host_broadcast(m_host, ch, packet);
+}
+
+void NetHost::flust(){
+    enet_host_flush(m_host);
+}
---
+++ net_host.hpp
@@ -0,0 +1,86 @@
+#pragma once
+
+#include<enet/enet.h>
+#include<cstdint>
+#include<vector>
+#include<tuple>
+#include<boost/noncopyable.hpp>
+#include<optional>
+#include<functional>
+
+using RecvData = std::vector<uint8_t>;
+using PacketCh = decltype(ENetEvent::channelID);
+
+
+/// @brief ホストの設定
+struct HostSetting {
+    /// @brief 最大接続数
+    size_t peer_max;
+    /// @brief 最大パケットチャンネル数
+    size_t packet_ch_max;
+    /// @brief 毎秒受信バイトサイズ
+    enet_uint32 down_size;
+    /// @brief 毎秒送信バイトサイズ
+    enet_uint32 up_size;
+    /// @brief ホストのアドレス
+    ENetAddress address;
+};
+
+class NetHost {
+    public:
+    /// @brief 既定のコンストラクタ
+    NetHost();
+
+    /// @brief デストラクタ
+    ~NetHost();
+
+    ///状態確認用演算子
+    ///@return エラーの有無を返す
+    operator bool();
+
+    /// @brief ホストを設定する
+    /// @param settings ホストの設定
+    /// @return 設定の成否
+    const bool set_host(const HostSetting& settings);
+
+    /// @brief ホストを削除する
+    void clear_host();
+
+    /// @brief 接続を要求する
+    /// @param dst 接続先のアドレス
+    /// @param max_ch 使用するチャンネルの数
+    /// @param data 接続先に与える値
+    /// @return 接続要求の成否
+    const bool request_connection(const ENetAddress* dst, const size_t max_ch, const enet_uint32 data);
+
+    /// @brief 目的の接続相手の有無を調べる
+    /// @param finder 接続相手かを答える関数
+    /// @return 有無の結果
+    const bool is_there_peer(const std::function<const bool(const ENetPeer*)>& finder);
+
+    /// @brief 目的の接続相手に対する処理を行う
+    /// @param finder 目的の接続相手かを答える関数
+    /// @param peer_handler 接続相手を扱う関数．目的の相手が見つかった時に呼び出される．
+    void handle_peer(
+        const std::function<const bool(const ENetPeer*)>& finder,
+        const std::function<void(ENetPeer*)>& peer_handler
+    );
+
+    ///ホストのイベントを処理する
+    ///@param event_handler イベントを処理する関数
+    ///@return イベント処理の結果
+    const bool handle_host_event(const std::function<const bool(const ENetEvent*)>& event_handler);
+
+    /// @brief 全接続先にパケットを送る
+    /// @param ch 送信チャンネル
+    /// @param data 送信パケット
+    /// @return 送信結果
+    void broadcast(const size_t ch, ENetPacket* packet);
+
+    ///データを実際に送る
+    void flust();
+
+    private:
+    /// @brief 接続先とやり取りするホスト
+    ENetHost *m_host;
+};
