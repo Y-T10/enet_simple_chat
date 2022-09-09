390cb4672ae099d6e257c90f17182aeb22dccf94
Y-T10 <tayuji111@gmail.com>
2022-09-09 19:30:04 +0900

    接続要求関数を変更し不要な関数を削除した

--- enet_client.hpp
+++ enet_client.hpp
@@ -14,14 +14,7 @@ class NetClient : public basic_enet {
     /// @brief 接続を要求する
     /// @param dst 接続先のアドレス
     /// @param max_ch 使用するチャンネルの数
-    /// @param timeout 接続要求がタイムアウト
+    /// @param data 接続先に渡す値
     /// @return 接続要求送信の成否
-    const bool request_connection(const ENetAddress& dst, const size_t max_ch, const enet_uint32 timeout);
-    
-    /// @brief 接続要求の結果を調べる
-    /// @return 接続要求の成否
-    const std::optional<bool> check_connection_result();
-    private:
-    /// @brief 接続要求の結果を受け取る
-    std::future<bool> m_request_result;
+    const bool request_connection(const ENetAddress& dst, const size_t max_ch, const enet_uint32 data);
 };
