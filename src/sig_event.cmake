9a068772202eeebd22832a8d76d31bd85f62a6c2
Y-T10 <tayuji111@gmail.com>
2022-09-17 20:21:41 +0900

    ライブラリのビルドを追加

--- src/sig_event.cmake
+++ src/sig_event.cmake
@@ -3,7 +3,11 @@ add_library(sig_event)
 
 #プログラムファイルを追加
 # - ${CMAKE_CURRENT_SOURCE_DIR}: このファイルがあるディレクトリ
+<<<<<<< HEAD
 target_sources(sig_event PRIVATE sig_event.cpp)
+=======
+target_sources(sig_event PRIVATE SigEvent.cpp)
+>>>>>>> 4c0df82 (ライブラリのビルドを追加)
 target_include_directories(sig_event PUBLIC ${CMAKE_CURRENT_SOURCE_DIR})
 
 #ライブラリを追加
