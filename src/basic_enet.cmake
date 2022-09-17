23e891bb9632608540ee28cc7122e3657b7390fa
Y-T10 <tayuji111@gmail.com>
2022-09-17 20:45:12 +0900

    enetライブラリの設定とファイル名を修正

--- src/basic_enet.cmake
+++ src/basic_enet.cmake
@@ -10,10 +10,6 @@ target_include_directories(basic_enet PUBLIC ${CMAKE_CURRENT_SOURCE_DIR})
 target_link_libraries(
     basic_enet PRIVATE
     cpp_common
-<<<<<<< HEAD
     PkgConfig::libenet
-=======
-    PkgConfig::enet
->>>>>>> 4c0df82 (ライブラリのビルドを追加)
     Boost::headers
 )
--- src/enet_packet_stream.cmake
+++ src/enet_packet_stream.cmake
@@ -3,21 +3,13 @@ add_library(enet_packet_stream)
 
 #プログラムファイルを追加
 # - ${CMAKE_CURRENT_SOURCE_DIR}: このファイルがあるディレクトリ
-<<<<<<< HEAD
 target_sources(enet_packet_stream PRIVATE enet_packet_stream.cpp)
-=======
-target_sources(enet_packet_stream PRIVATE console_io.cpp)
->>>>>>> 4c0df82 (ライブラリのビルドを追加)
 target_include_directories(enet_packet_stream PUBLIC ${CMAKE_CURRENT_SOURCE_DIR})
 
 #ライブラリを追加
 target_link_libraries(
     enet_packet_stream PRIVATE
     cpp_common
-<<<<<<< HEAD
     PkgConfig::libenet
-=======
-    PkgConfig::enet
->>>>>>> 4c0df82 (ライブラリのビルドを追加)
     msgpackc-cxx
 )
