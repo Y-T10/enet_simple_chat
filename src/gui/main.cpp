7e69f5490d26f456aefa26dbfbe160f093693b24
Y-T10 <t06t06ca2@gmail.com>
2023-10-08 20:20:08 +0900

    メインの実装を変更

--- src/gui/main.cpp
+++ src/gui/main.cpp
@@ -3,6 +3,9 @@
 #include <SDL2/SDL_events.h>
 #include <SDL2/SDL_video.h>
 
+#include "title.hpp"
+#include "SceneManager.hpp"
+
 static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
 
@@ -28,58 +31,6 @@ static int setup_program(int argc, char **argv)
     return 0;
 }
 
-static void mainloop(void)
-{
-    SDL_Rect mouseposrect;
-    Uint8 r;
-    SDL_bool keep_going = SDL_TRUE;
-    SDL_Event event;
-
-    mouseposrect.x = mouseposrect.y = -1000;  /* -1000 so it's offscreen at start */
-    mouseposrect.w = mouseposrect.h = 50;
-
-    /* run the program until told to stop. */
-    while (keep_going) {
-
-        /* run through all pending events until we run out. */
-        while (SDL_PollEvent(&event)) {
-            switch (event.type) {
-                case SDL_QUIT:  /* triggers on last window close and other things. End the program. */
-                    keep_going = SDL_FALSE;
-                    break;
-
-                case SDL_KEYDOWN:  /* quit if user hits ESC key */
-                    if (event.key.keysym.sym == SDLK_ESCAPE) {
-                        keep_going = SDL_FALSE;
-                    }
-                    break;
-
-                case SDL_MOUSEMOTION:  /* keep track of the latest mouse position */
-                    /* center the square where the mouse is */
-                    mouseposrect.x = event.motion.x - (mouseposrect.w / 2);
-                    mouseposrect.y = event.motion.y - (mouseposrect.h / 2);
-                    break;
-            }
-        }
-
-        /* fade between shades of red every 3 seconds, from 0 to 255. */
-        r = (Uint8) ((((float) (SDL_GetTicks() % 3000)) / 3000.0f) * 255.0f);
-        SDL_SetRenderDrawColor(renderer, r, 0, 0, 255);
-
-        /* you have to draw the whole window every frame. Clearing it makes sure the whole thing is sane. */
-        SDL_RenderClear(renderer);  /* clear whole window to that fade color. */
-
-        /* set the color to white */
-        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-
-        /* draw a square where the mouse cursor currently is. */
-        SDL_RenderFillRect(renderer, &mouseposrect);
-
-        /* put everything we drew to the screen. */
-        SDL_RenderPresent(renderer);
-    }
-}
-
 static void shutdown_program(void)
 {
     SDL_DestroyRenderer(renderer);
@@ -87,15 +38,30 @@ static void shutdown_program(void)
     SDL_Quit();
 }
 
+using MainSceneMgr = SceneMgr<SceneTitle>;
+
+struct SwitcherMainScene {
+    void operator()(MainSceneMgr& mgr, SceneTitle& scene){
+        mgr.setFinishFlag(true);
+    };
+};
 
 /* this is always a main() function, even if you're on a platform that uses
    something else. SDL_main.h takes care of figuring out the details and
    making sure the program starts here. */
-int main(int argc, char **argv)
-{
-    if (setup_program(argc, argv) == 0) {
-        mainloop();
+int main(int argc, char **argv) {
+    if (setup_program(argc, argv) != 0) {
+        shutdown_program();
+        return -1;
     }
+
+    MainSceneMgr mgr;
+    mgr.setCurrentScene(SceneTitle());
+    while(mgr.doesContinue()) {
+        mgr.executeScene(window, renderer);
+        mgr.switchScene<SwitcherMainScene>();
+    }
+    
     shutdown_program();
     return 0;
 }
